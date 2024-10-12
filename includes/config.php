1d4ff000cc5e067e58f3f78be7f15b119ad6e46c
benjaminmendezinintech <benjaminmendezinintech@gmail.com>
2024-10-12 03:17:07 -0600

    Repo11

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'http://34.29.9.49/');
+define('DB_HOST', '34.29.9.49');
 define('DB_USER', 'bmadev');
 define('DB_PASS', 'd3v1a');
 define('DB_NAME', 'PRUEBA_DB_CAJA_FINTECH');
